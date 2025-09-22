db6575d537c27bca05f49df12344eb2d0b2485c7
Your Name <you@example.com>
2025-09-22 16:54:26 +0100

    `Added cors configuration with allowed headers and credentials`

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+// ...existing code...
 
 app.get('/', (req, res) => {
   res.send('Hello TechyJAunt Cohort 6, welcome to my student management API!');
